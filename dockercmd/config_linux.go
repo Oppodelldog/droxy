32e9171f9ea5969bc75e519dca0222df7fa29114
Oppodelldog <gittyD0g>
2018-02-11 18:51:37 +0100

    first steps for windows support

--- dockercmd/config_linux.go
+++ dockercmd/config_linux.go
@@ -4,7 +4,7 @@ import "github.com/Oppodelldog/docker-proxy-command/config"
 
 func addGroups(commandDef *config.CommandDefinition, builder *Builder) error {
 	if addGroups, ok := commandDef.GetAddGroups(); ok {
-		err = buildGroups(addGroups, builder)
+		err := buildGroups(addGroups, builder)
 		if err != nil {
 			return err
 		}
