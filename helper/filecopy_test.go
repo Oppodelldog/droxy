28cfcbf538feee1f2307ea8963a6b23e71cba02d
Oppodelldog <odog@netcologne.de>
2018-03-15 12:11:28 +0100

    more tests #7

---
+++ helper/filecopy_test.go
@@ -0,0 +1,50 @@
+package helper
+
+import (
+	"testing"
+	"io/ioutil"
+	"os"
+	"path"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopyFile(t *testing.T) {
+
+	testFolder := "/tmp/droxy/fileCopyTest/"
+	os.MkdirAll(testFolder, 0776)
+
+	srcBytes := []byte("HELLO DROXY!!")
+	src := path.Join(testFolder, "fileToCopy")
+	err := ioutil.WriteFile(src, srcBytes, 0666)
+	if err != nil {
+		t.Fatalf("Did not expect ioutil.WriteFile to return an error, but got: %v", err)
+	}
+
+	dst := path.Join(testFolder, "fileCopied")
+
+	err = CopyFile(src, dst)
+	if err != nil {
+		t.Fatalf("Did not expect CopyFile to return an error, but got: %v", err)
+	}
+
+	dstBytes, err := ioutil.ReadFile(dst)
+	if err != nil {
+		t.Fatalf("Did not expect ioutil.ReadFile to return an error, but got: %v", err)
+	}
+
+	assert.Equal(t, dstBytes, srcBytes)
+
+	err = os.RemoveAll(testFolder)
+	if err != nil {
+		t.Fatalf("Did not expect os.Remove to return an error, but got: %v", err)
+	}
+}
+
+func TestCopyFile_srcFileDoesNotExist_expectError(t *testing.T) {
+
+	src := "/tmp/THIS_FILE_DOES_NOT_EXIST"
+	dst := ""
+	err := CopyFile(src, dst)
+
+	assert.Error(t, err)
+}
---
+++ helper/filesystem_test.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetExecutablePath(t *testing.T) {
+	ex, err := os.Executable()
+	if err != nil {
+		t.Fatalf("Did not expect os.Executable() to return an error, but got: %v", err)
+	}
+
+	executablePath, err := GetExecutablePath()
+	if err != nil {
+		t.Fatalf("Did not expect GetExecutablePath() to return an error, but got: %v", err)
+	}
+
+	assert.Equal(t, filepath.Dir(ex), executablePath)
+}
+
+func TestGetExecutableFilePath_smoketest(t *testing.T) {
+	GetExecutableFilePath()
+}
