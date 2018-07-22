c1593842257958ed42bbb20b33d7a412144c4854
Oppodelldog <odog@netcologne.de>
2018-07-22 11:42:46 +0200

    update build.sh with test mode

--- .release/build.sh
+++ .release/build.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
+if [ $1 = "test" ]; then
+    tag="v0.0.0"
+else
+    workingBranch=$(git rev-parse --abbrev-ref HEAD)
 
-workingBranch=$(git rev-parse --abbrev-ref HEAD)
+    cleanWorkspace=$(git diff --exit-code)
+    if [ $? -ne 0 ]; then
+        echo "git workspace is not clean, commit or stash your changes"
+        exit 1
+    fi
 
-cleanWorkspace=$(git diff --exit-code)
-if [ $? -ne 0 ]; then
-    echo "git workspace is not clean, commit or stash your changes"
-    exit 1
+    git fetch --tags
+    tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+    if [ -z ${tag} ]; then
+        echo "could not find latest tag"
+        exit 1
+    fi
+
+    git checkout ${tag}
 fi
-
-git fetch --tags
-tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-if [ -z $tag ]; then
-    echo "could not find latest tag"
-    exit 1
-fi
-
-git checkout $tag
 
 target_folder=".release"
 package="github.com/Oppodelldog/droxy"
@@ -24,20 +27,27 @@ ldflags=-ldflags="-X github.com/Oppodelldog/droxy/version.Number=${tag}"
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 
-platforms=("linux/amd64" "windows/amd64" "windows/386")
+platforms=("linux/amd64" "windows/amd64" "windows/386" "linux/arm/7")
 
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_folder="${GOOS}-${GOARCH}"
-    output_name=$package_name
-    if [ $GOOS = "windows" ]; then
+    GOARM=
+
+    if [ ${GOARCH} = "arm" ]; then
+        GOARM=${platform_split[2]}
+
+    fi
+
+    output_folder="${GOOS}-${GOARCH}${GOARM}"
+    output_name=${package_name}
+    if [ ${GOOS} = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build "${ldflags}" -o "${target_folder}/${tag}/${output_folder}/${output_name}" $package
+    env GOOS=${GOOS} GOARCH=${GOARCH} GOARM=${GOARM} go build "${ldflags}" -o "${target_folder}/${tag}/${output_folder}/${output_name}" ${package}
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
@@ -49,9 +59,12 @@ do
 
     tar -cvzf ../${output_name}-${output_folder}.tar.gz ${output_name}
 
-    cd $currentWd
+    cd ${currentWd}
     rm -rf ${target_folder}/${tag}/${output_folder}
 done
 
-
-git checkout ${workingBranch}
+if [ $1 = "test" ]; then
+    echo "test release done"
+else
+    git checkout ${workingBranch}
+fi
