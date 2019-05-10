5bd171c09e87dd8629304fb811045f6fb527c90a
Oppodelldog <odog@netcologne.de>
2019-05-11 00:46:59 +0200

    let failing tests fail the build

--- .test/run.sh
+++ .test/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export DROXY_FILE_SIZE=$( stat --printf="%s" droxy)
+set -euxo pipefail
 
+export DROXY_FILE_SIZE=$( stat --printf="%s" droxy)
 
 venom run tests.yml --env true 
