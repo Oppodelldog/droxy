3e59ae45f8e9ae72243c947add9ef4284f9b2095
Oppodelldog <odog@netcologne.de>
2020-06-09 21:16:01 +0200

    fix linter issue: align duplicate code into one file

--- proxyfile/linksstrategy.go
+++ proxyfile/linksstrategy.go
@@ -4,6 +4,25 @@ import (
 	"os"
 )
 
+// NewHardlinkStrategy creates a new FileCreationStrategy that produces hardlinks of droxy command.
+func NewHardlinkStrategy() FileCreationStrategy {
+	return &HardlinkStrategy{
+		hardLinkFunction: os.Link,
+	}
+}
+
+//HardlinkStrategy contains the implementation of creating a hardlink to droxy executable.
+type HardlinkStrategy struct {
+	hardLinkFunction hardlinkFunctionDef
+}
+
+type hardlinkFunctionDef func(string, string) error
+
+//CreateProxyFile creates a hardlink from commandNameFilePath to commandBinaryFilePath.
+func (s *HardlinkStrategy) CreateProxyFile(commandBinaryFilePath, commandNameFilePath string) error {
+	return s.hardLinkFunction(commandBinaryFilePath, commandNameFilePath)
+}
+
 // NewSymlinkStrategy creates a new FileCreationStrategy that produces symlinks of droxy command.
 func NewSymlinkStrategy() FileCreationStrategy {
 	return &SymlinkStrategy{
--- proxyfile/linksstrategy_test.go
+++ proxyfile/linksstrategy_test.go
@@ -49,3 +49,44 @@ func TestNewHardlinkStrategy_returnsErrorIfSystemFunctionReturnsError(t *testing
 
 	assert.Equal(t, expectedError, err)
 }
+
+func TestNewSymlinkStrategy_callsTheAppropriateSystemFunction(t *testing.T) {
+	strategy := NewSymlinkStrategy()
+
+	strategyFunction := strategy.(*SymlinkStrategy).symlinkFunction
+	expectedFunction := os.Symlink
+
+	if reflect.ValueOf(expectedFunction).Pointer() != reflect.ValueOf(strategyFunction).Pointer() {
+		t.Fail()
+	}
+}
+
+func TestNewSymlinkStrategy_callsConfiguredSystemFunction(t *testing.T) {
+	mock := fileCreationFunctionMock{}
+	strategy := NewSymlinkStrategy()
+	strategy.(*SymlinkStrategy).symlinkFunction = mock.FileCreationFunc
+
+	expectedSrc := "A"
+	expectedDst := "B"
+
+	err := strategy.CreateProxyFile(expectedSrc, expectedDst)
+	if err != nil {
+		t.Fatalf("Did not expect CreateProxyFile to return an error, but got: %v", err)
+	}
+
+	assert.Equal(t, expectedSrc, mock.parmSrc)
+	assert.Equal(t, expectedDst, mock.parmDst)
+	assert.Equal(t, 1, mock.calls)
+}
+
+func TestNewSymlinkStrategy_returnsErrorIfSystemFunctionReturnsError(t *testing.T) {
+	expectedError := errors.New("error from configured system function")
+
+	mock := fileCreationFunctionMock{returnValue: expectedError}
+	strategy := NewSymlinkStrategy()
+	strategy.(*SymlinkStrategy).symlinkFunction = mock.FileCreationFunc
+
+	err := strategy.CreateProxyFile("A", "B")
+
+	assert.Equal(t, expectedError, err)
+}
