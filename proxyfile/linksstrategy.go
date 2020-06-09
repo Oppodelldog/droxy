ba790f635d08012f1cb43ff4290d3e389b460064
Oppodelldog <odog@netcologne.de>
2020-06-09 21:45:38 +0200

    fix linter issues: remove duplicate code

--- proxyfile/linksstrategy.go
+++ proxyfile/linksstrategy.go
@@ -4,40 +4,24 @@ import (
 	"os"
 )
 
-// NewHardlinkStrategy creates a new FileCreationStrategy that produces hardlinks of droxy command.
-func NewHardlinkStrategy() FileCreationStrategy {
-	return &HardlinkStrategy{
-		hardLinkFunction: os.Link,
+// NewHardlinkStrategy creates a new FsLinkCreator that produces hardlinks of droxy command.
+func NewHardlinkStrategy() FsLinkCreator {
+	return FsLinkCreator{
+		strategy: os.Link,
 	}
 }
 
-//HardlinkStrategy contains the implementation of creating a hardlink to droxy executable.
-type HardlinkStrategy struct {
-	hardLinkFunction hardlinkFunctionDef
-}
-
-type hardlinkFunctionDef func(string, string) error
-
-//CreateProxyFile creates a hardlink from commandNameFilePath to commandBinaryFilePath.
-func (s *HardlinkStrategy) CreateProxyFile(commandBinaryFilePath, commandNameFilePath string) error {
-	return s.hardLinkFunction(commandBinaryFilePath, commandNameFilePath)
-}
-
-// NewSymlinkStrategy creates a new FileCreationStrategy that produces symlinks of droxy command.
-func NewSymlinkStrategy() FileCreationStrategy {
-	return &SymlinkStrategy{
-		symlinkFunction: os.Symlink,
+// NewSymlinkStrategy creates a new FsLinkCreator that produces symlinks of droxy command.
+func NewSymlinkStrategy() FsLinkCreator {
+	return FsLinkCreator{
+		strategy: os.Symlink,
 	}
 }
 
-//SymlinkStrategy contains the implementation of creating a symlink to droxy executable.
-type SymlinkStrategy struct {
-	symlinkFunction symlinkFunctionDef
+type FsLinkCreator struct {
+	strategy func(string, string) error
 }
 
-type symlinkFunctionDef func(string, string) error
-
-//CreateProxyFile creates a symlink from commandNameFilePath to commandBinaryFilePath.
-func (s *SymlinkStrategy) CreateProxyFile(commandBinaryFilePath, commandNameFilePath string) error {
-	return s.symlinkFunction(commandBinaryFilePath, commandNameFilePath)
+func (c FsLinkCreator) CreateProxyFile(commandBinaryFilePath, commandNameFilePath string) error {
+	return c.strategy(commandBinaryFilePath, commandNameFilePath)
 }
--- proxyfile/linksstrategy_test.go
+++ proxyfile/linksstrategy_test.go
@@ -9,81 +9,76 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewHardlinkStrategy_configuresTheAppropriateSystemFunction(t *testing.T) {
-	strategy := NewHardlinkStrategy()
+func TestLinkStrategies_configuresTheAppropriateSystemFunction(t *testing.T) {
+	testCases := map[string]struct {
+		linkCreator      FsLinkCreator
+		expectedFunction func(string, string) error
+	}{
+		"hardlink": {
+			linkCreator:      NewHardlinkStrategy(),
+			expectedFunction: os.Link,
+		},
+		"symlink": {
+			linkCreator:      NewSymlinkStrategy(),
+			expectedFunction: os.Symlink,
+		},
+	}
 
-	strategyFunction := strategy.(*HardlinkStrategy).hardLinkFunction
-	expectedFunction := os.Link
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			want := reflect.ValueOf(testCase.expectedFunction).Pointer()
+			got := reflect.ValueOf(testCase.linkCreator.strategy).Pointer()
 
-	if reflect.ValueOf(expectedFunction).Pointer() != reflect.ValueOf(strategyFunction).Pointer() {
-		t.Fail()
+			if want != got {
+				t.Fatalf("want: %v, got: %v", want, got)
+			}
+		})
 	}
 }
-func TestNewSymlinkStrategy_callsTheAppropriateSystemFunction(t *testing.T) {
-	strategy := NewSymlinkStrategy()
 
-	strategyFunction := strategy.(*SymlinkStrategy).symlinkFunction
-	expectedFunction := os.Symlink
-
-	if reflect.ValueOf(expectedFunction).Pointer() != reflect.ValueOf(strategyFunction).Pointer() {
-		t.Fail()
+func TestFsLinkCreator_callsConfiguredSystemFunction(t *testing.T) {
+	mock := &linkMock{}
+	linkCreator := FsLinkCreator{
+		strategy: mock.Call,
 	}
-}
-
-func TestNewHardlinkStrategy_callsConfiguredSystemFunction(t *testing.T) {
-	mock := fileCreationFunctionMock{}
-	strategy := NewHardlinkStrategy()
-	strategy.(*HardlinkStrategy).hardLinkFunction = mock.FileCreationFunc
 
 	expectedSrc := "A"
 	expectedDst := "B"
 
-	err := strategy.CreateProxyFile(expectedSrc, expectedDst)
+	err := linkCreator.CreateProxyFile(expectedSrc, expectedDst)
 	if err != nil {
 		t.Fatalf("Did not expect CreateProxyFile to return an error, but got: %v", err)
 	}
 
-	assert.Equal(t, expectedSrc, mock.parmSrc)
-	assert.Equal(t, expectedDst, mock.parmDst)
+	assert.Equal(t, expectedSrc, mock.arg1)
+	assert.Equal(t, expectedDst, mock.arg2)
 	assert.Equal(t, 1, mock.calls)
 }
-func TestNewSymlinkStrategy_callsConfiguredSystemFunction(t *testing.T) {
-	mock := fileCreationFunctionMock{}
-	strategy := NewSymlinkStrategy()
-	strategy.(*SymlinkStrategy).symlinkFunction = mock.FileCreationFunc
 
-	expectedSrc := "A"
-	expectedDst := "B"
+func TestFsLinkCreator_returnsErrorIfSystemFunctionReturnsError(t *testing.T) {
+	expectedError := errors.New("error from configured system function")
 
-	err := strategy.CreateProxyFile(expectedSrc, expectedDst)
-	if err != nil {
-		t.Fatalf("Did not expect CreateProxyFile to return an error, but got: %v", err)
+	mock := &linkMock{err: expectedError}
+	linkCreator := FsLinkCreator{
+		strategy: mock.Call,
 	}
 
-	assert.Equal(t, expectedSrc, mock.parmSrc)
-	assert.Equal(t, expectedDst, mock.parmDst)
-	assert.Equal(t, 1, mock.calls)
-}
-
-func TestNewHardlinkStrategy_returnsErrorIfSystemFunctionReturnsError(t *testing.T) {
-	expectedError := errors.New("error from configured system function")
-
-	mock := fileCreationFunctionMock{returnValue: expectedError}
-	strategy := NewHardlinkStrategy()
-	strategy.(*HardlinkStrategy).hardLinkFunction = mock.FileCreationFunc
-
-	err := strategy.CreateProxyFile("A", "B")
+	err := linkCreator.CreateProxyFile("A", "B")
 
 	assert.Equal(t, expectedError, err)
 }
-func TestNewSymlinkStrategy_returnsErrorIfSystemFunctionReturnsError(t *testing.T) {
-	expectedError := errors.New("error from configured system function")
 
-	mock := fileCreationFunctionMock{returnValue: expectedError}
-	strategy := NewSymlinkStrategy()
-	strategy.(*SymlinkStrategy).symlinkFunction = mock.FileCreationFunc
+type linkMock struct {
+	arg1  string
+	arg2  string
+	calls int
+	err   error
+}
 
-	err := strategy.CreateProxyFile("A", "B")
+func (m *linkMock) Call(p1, p2 string) error {
+	m.arg1 = p1
+	m.arg2 = p2
+	m.calls++
 
-	assert.Equal(t, expectedError, err)
+	return m.err
 }
